42783b387b9124b4205945f977f211583d461fb9
JnsMT <hessjannis@gmail.com>
2021-03-12 11:05:48 +0100

    Implementierung eines anfänglichen LadeScreens

---
+++ LightVisionSettings/Loading.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LightVisionSettings
+{
+    public partial class Loading : Form
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+        static Form loadingForm;
+        private delegate void CloseDelegate();
+        
+    
+        static public void ShowLoadingScreen()
+        {
+            loadingForm = new Loading();
+            Thread t = new Thread(new ThreadStart(Loading.ShowForm));
+            t.Start();
+        }
+
+        static private void ShowForm()
+        {
+            if(loadingForm != null)
+            {
+                Application.Run(loadingForm);
+            }
+        }
+
+        static public void CloseForm()
+        {
+            loadingForm.Invoke(new CloseDelegate(CloseFormInternal));
+        }
+
+        static private void CloseFormInternal()
+        {
+            if(loadingForm != null)
+            {
+                loadingForm.Close();
+                loadingForm = null;
+            }
+        }
+    }
+}
