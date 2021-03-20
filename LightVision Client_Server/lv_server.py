7c688125b8b2ca261d3ee89d37e7f6441aa35465
JnsMT <57483606+JnsMT@users.noreply.github.com>
2021-03-20 19:32:52 +0100

    Timestamp und Sicherheitsverbesserung

--- LightVision Client_Server/lv_server.py
+++ LightVision Client_Server/lv_server.py
@@ -2,6 +2,7 @@ import socket
 import json
 from threading import Thread
 from time import sleep
+from datetime import datetime
 
 HOST = '135.181.35.212'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -10,7 +11,8 @@ panel = None
 
 def handleConn(conn, addr):
     global panel
-    print(addr)
+    timestamp = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    print(f"Conn: {addr}, Time: {timestamp}")
     while True:
         continue_read = True
         buffer = ""
@@ -25,9 +27,10 @@ def handleConn(conn, addr):
             conn.send(bytes(panel + "\n", encoding="utf-8"))
         else:
             conn.send(bytes("set\n", encoding="utf-8"))
-            panel = buffer
-            t = Thread(target=savePanelsMan, daemon=True)
-            t.start()
+            if buffer != None and buffer != "":
+                panel = buffer
+                t = Thread(target=savePanelsMan, daemon=True)
+                t.start()
 
 def openServer():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -37,14 +40,6 @@ def openServer():
             conn, addr = s.accept()
             t = Thread(target=handleConn, args=(conn, addr), daemon=True)
             t.start()
-
-def savePanelsAuto():
-    while True:
-        if panel != None and panel != "":
-            file = open("savedPanels.txt", "w")
-            file.write(panel)
-            file.close()
-            sleep(30)
             
 def savePanelsMan():
     if panel != None and panel != "":
@@ -60,6 +55,4 @@ def loadPanelsOnStart():
     file.close()
 
 loadPanelsOnStart()
-t = Thread(target=savePanelsAuto, daemon=True)
-t.start()
 openServer()
